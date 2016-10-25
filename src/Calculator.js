af9fcfd206c44cdfc3c84ec6f499d8bdd8edf5d4
evzheniy <evzheniy@ya.ru>
2016-10-25 15:27:48 +0300

    add main component

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="Calculator">
+        <h1>React Calculator</h1>
+
+      </div>
+    )
+  }
+}
+
+export default Calculator
